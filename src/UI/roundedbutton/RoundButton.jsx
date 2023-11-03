5773a197fd9c611eb646d666878d6031846f1e95
trdxlucifer <singhdepu566@gmail.com>
2023-11-03 18:53:13 +0530

    cone

--- src/UI/roundedbutton/RoundButton.jsx
+++ src/UI/roundedbutton/RoundButton.jsx
@@ -5,9 +5,12 @@ import {CaretDownFilled  } from '@ant-design/icons'
 const RoundButton = () => {
   return (
     <div className='btn-div'>
+      <div className="btn-container">
       <button className='btn'>Save and continue</button>
       <div className='btn-right'>
         <CaretDownFilled />
+      </div>
+
       </div>
     </div>
   )
