891eaa70bf1488b4052d0f195e6a16a04d6d9818
trdxlucifer <singhdepu566@gmail.com>
2023-11-17 14:19:19 +0530

    comit

--- src/pages/signup/SignupPage.jsx
+++ src/pages/signup/SignupPage.jsx
@@ -1,105 +1,116 @@
-import { Button, Image, Typography } from "antd";
-import "./signup.css";
-import { Form, Input } from "antd";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Button, Image, Typography } from 'antd'
+import './signup.css'
+import { Form, Input } from 'antd'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const SignupPage = () => {
-  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false)
 
-  function handleSubmit(e){
+  function handleSubmit (e) {
     e.preventDefault()
-    alert("work")
+    alert('work')
   }
   return (
     <>
-      <div className="signup-container">
-        <div className="signup-logo">
-          <Image preview={false} src="/waveLogo.png" height={35} width={120} />
+      <div className='signup-container'>
+        <div className='signup-logo'>
+          <Image preview={false} src='/waveLogo.png' height={35} width={120} />
         </div>
-        <div className="signup-header">
-          <h1 className="signup-header-title">Check out Wave — it’s free!</h1>
-          <h2 className="signup-header-subtitle">
+        <div className='signup-header'>
+          <h1 className='signup-header-title'>Check out Wave — it’s free!</h1>
+          <h2 className='signup-header-subtitle'>
             Wave helps freelancers, consultants, and small businesses <br />
             around the world simplify their finances.
           </h2>
 
-          <div className="signup-form">
-            <Form >
-              <div className="text-email">
+          <div className='signup-form'>
+            <Form onFinish={handleSubmit}>
+              <div className='text-email'>
                 <Input
-                
                   style={{
-                    marginTop: "5px",
-                    width: "100%",
-                    fontStyle: "italic",
-                    fontWeight: "600",
+                    marginTop: '5px',
+                    width: '100%',
+                    fontStyle: 'italic',
+                    fontWeight: '600'
                   }}
-                  type="text"
-                  size="large"
-                  placeholder="Email"
+                  type='text'
+                  size='large'
+                  placeholder='Email'
                   rules={[
                     {
                       required: true,
-                      message: 'Please input your username!',
-                    },
+                      message: 'Please input your username!'
+                    }
                   ]}
                 />
               </div>
-              <div className="text-password">
+              <div className='text-password'>
                 <Input.Password
                   style={{
-                    marginTop: "5px",
-                    width: "100%",
-                    fontStyle: "italic",
-                    fontWeight: "600",
+                    marginTop: '5px',
+                    width: '100%',
+                    fontStyle: 'italic',
+                    fontWeight: '600'
                   }}
-                  type="text"
-                  size="large"
-                  placeholder="Password"
+                  type='text'
+                  size='large'
+                  placeholder='Password'
                   visibilityToggle={{
                     visible: passwordVisible,
-                    onVisibleChange: setPasswordVisible,
+                    onVisibleChange: setPasswordVisible
                   }}
                 />
 
-                <div className="anchor-div_span">
+                <div className='anchor-div_span'>
                   <span
-                    href="#"
-                    className="anchor-link"
-                    onClick={() =>
-                      setPasswordVisible((prevState) => !prevState)
-                    }
+                    href='#'
+                    className='anchor-link'
+                    onClick={() => setPasswordVisible(prevState => !prevState)}
                   >
-                    {passwordVisible ? "Hide" : "Show"}
+                    {passwordVisible ? 'Hide' : 'Show'}
                   </span>
                 </div>
-                <button className="submit-btn" type="submit">Sign in</button>
+                <Button
+                  size='large'
+                  shape='round'
+                  style={{
+                    width: '100%',
+                    marginTop: '20px',
+                    boxShadow:
+                      '0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25)'
+                  }}
+                >
+                  Sign in
+                </Button>
+                {/*   <Button className='submit-btn' type='submit'>
+                  Sign in
+                </Button> */}
               </div>
-              <div className="line">
-                <span className="line-line"></span>
-                <span id="text">or</span>
-                <span className="line-line"></span>
+              <div className='line'>
+                <span className='line-line'></span>
+                <span id='text'>or</span>
+                <span className='line-line'></span>
               </div>
-              <button className="google-btn">
+              <button className='google-btn'>
                 <span>Sign in with Google</span>
               </button>
             </Form>
           </div>
 
-          <div className="footer-text">
-            <p className="footer-p">
+          <div className='footer-text'>
+            <p className='footer-p'>
               By continuing, you are indicating that you have read and agree to
               the
               <span> Terms of Use </span>
               and
               <span> Privacy Policy </span>
             </p>
-            <div className="footer-text-2">
-              <p className="footer-p">
+            <div className='footer-text-2'>
+              <p className='footer-p'>
                 Don&apos;t have a Wave account yet?
-                <Link to="/login">
-                  <span id="span_signup"> Sign in now.</span>
+                <Link to='/login'>
+                  <span id='span_signup'> Sign in now.</span>
                 </Link>
               </p>
             </div>
@@ -107,7 +118,7 @@ const SignupPage = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SignupPage;
+export default SignupPage
