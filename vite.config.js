c5710aa445920a82143bb3e965dda75cd4510a13
trdxlucifer <singhdepu566@gmail.com>
2023-11-29 17:11:20 +0530

    made changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import viteCompression from 'vite-plugin-compression';
 
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import react from 'vite-plugin-react'; // Add this import
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteCompression() ],
-})
+  plugins: [react(), viteCompression()],
+});
